a6245c39ec130078064a1cc74023417be7fbe52a
abhilashajayan2001 <abhilashajayan2001@gmail.com>
2024-01-20 14:00:46 +0530

    register done

---
+++ user-service/src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import nocache from 'nocache';
+import { rabbitmq } from './infra/rabbitmq'
+import { UserUsecase } from './usecases/userUsecase';
+import { userRepository } from './repository/userRepo';
+import { UserModel } from './adapters/userSchema';
+import { dbConnection } from './config/database.config';
+
+const userRepositorys = new userRepository(UserModel);
+const userUsecases = new UserUsecase(userRepositorys);
+const consumerMessage = new rabbitmq(userUsecases);
+
+
+const app = express();
+const port =  process.env.PORT || 3001;
+app.use(cors());
+
+app.use(nocache());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+dbConnection();
+const rabbitMQ = async () => {
+    await consumerMessage.userRegConsumer()
+  };
+rabbitMQ();
+
+app.listen(port, () => {
+    console.log(`Server Running on ${port}`);
+  });
